8944e63a7a52132e0cb5d57039769a9ca6abe6cc
akrom067 <raxmatullayevakrom1@gmail.com>
2026-05-13 21:01:22 +0500

    feat: add new service.py

---
+++ service.py
@@ -0,0 +1,47 @@
+import json
+
+from .main import User
+
+def authenticase(user_id: str):
+
+    try:
+        with open("users_table.json", mode="r", encoding="utf-8") as f:
+            users = json.load(f)
+
+    except FileNotFoundError:
+        return None
+
+    user = users.get[user_id]
+    if not user:
+        return None
+
+    return User(**user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
